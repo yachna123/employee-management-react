73fa22d0cad09c3adae9cfebf71898d083388ffb
Krina Khunt <krinakhunt12@gmail.com>
2026-02-01 19:20:49 +0530

    Qr code generate

--- Birthday/src/App.jsx
+++ Birthday/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import QRCodePage from './components/QRCodePage';
 import CountdownPage from './components/CountdownPage';
 import BirthdayPage from './components/BirthdayPage';
 import MemoriesPage from './components/MemoriesPage';
@@ -9,8 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<QRCodePage />} />
-        <Route path="/countdown" element={<CountdownPage />} />
+        <Route path="/" element={<CountdownPage />} />
         <Route path="/birthday" element={<BirthdayPage />} />
         <Route path="/memories" element={<MemoriesPage />} />
       </Routes>
